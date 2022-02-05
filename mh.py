1aee86c2adcc09d71f3482772d25b3861d4fdea9
IgorPietrzak <96392306+IgorPietrzak@users.noreply.github.com>
2022-02-05 13:53:39 +0000

    Add files via upload

---
+++ mh.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def a(n):
+    result = (5-77*np.sin(n)+8*n**2)/(1-4*n**2)
+    return result
+
+def plot():
+    x = np.arange(1,15,0.5) # Half open interval
+    y= a(x)
+    plt.plot(x,y,'ro') # red- 'r', blue- 'b'{solid lines}, add o at the end for dots.
+    plt.show()
+
+def parta():
+    i = 0
+    while i < 1000:
+        print(a(i))
+        i += 1
+    plot()
+
+def partb():
+    L = -2.0
+    eps = 1/500
+    N = 0
+
+    while abs(a(N) - L) >= eps:
+        N += 1
+    print(str(N))
+
+def partc():
+    M = 1000
+    L = -2.0
+    eps = 1/500
+    while abs(a(M) - L) < eps:
+        M -= 1
+    ans = M + 1
+    print(str(ans))
+
+
+def main():
+    partc()
+
+main()
