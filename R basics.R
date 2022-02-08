3533b6e96f81150683c2f30f535ef8f011eb4e14
IgorPietrzak <96392306+IgorPietrzak@users.noreply.github.com>
2022-02-08 23:28:26 +0000

    Add files via upload

--- R basics.R
+++ R basics.R
@@ -1,6 +1,6 @@
 data = read.csv('file:///Users/igorpietrzak/Downloads/R2019.csv')
 table = table(data$How.would.you.describe.your.programming.skills.,
-              data$How.much.have.you.enjoyed.using.R.so.far., useNA = 'always')
+              data$How.much.have.you.enjoyed.using.R.so.far.)
 View(table)
 
 
