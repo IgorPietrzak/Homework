bfa5b59cd1b7574fc7a35737a91b3e835f6b9d86
IgorPietrzak <igor.pietrzak70@gmail.com>
2022-03-11 11:03:24 +0000

    formatting

--- project2.r
+++ project2.r
@@ -3,12 +3,12 @@ data_full <- read.csv("http://www1.maths.leeds.ac.uk/~arief/MATH1712/data/vessel
 data_subset <- subset(data_full, select = -c(Licence.Category)) # Create subset of data_full without Licence.Category column. # nolint
 data <- unique(data_subset) # Remove excess rows
 
-how_many_duplicates <- function(){
+how_many_duplicates <- function() {
     duplicates <- nrow(data_subset) - nrow(data)
     return(duplicates)
 }
 
-most_common_vessel_name <- function(){
+most_common_vessel_name <- function() {
     name <- names(which.max(table(data$Vessel.name)))
     return(name)
 }
