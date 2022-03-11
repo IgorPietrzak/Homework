91495a51cd5fa3d62dbb50bb1dd5c9946fccb017
IgorPietrzak <igor.pietrzak70@gmail.com>
2022-03-11 10:42:01 +0000

    Task 1 update

--- project2.r
+++ project2.r
@@ -1,5 +1,14 @@
+# TASK 1:
 data_full <- read.csv("http://www1.maths.leeds.ac.uk/~arief/MATH1712/data/vessels.csv") # nolint
 data_subset <- subset(data_full, select = -c(Licence.Category)) # Create subset of data_full without Licence.Category column. # nolint
 data <- unique(data_subset) # Remove excess rows
-most_common_vessel_name <- names(which.max(table(data$Vessel.name))) # get most common vessel name. # nolint
-print(most_common_vessel_name)
+
+how_many_duplicates <- function(){
+    duplicates <- nrow(data_subset) - nrow(data)
+    return(duplicates)
+}
+
+most_common_vessel_name <- function(){
+    name <- names(which.max(table(data$Vessel.name)))
+    return(name)
+}
