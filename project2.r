7f02ece93828c46fe9bd4cf92c06e72895fdf941
IgorPietrzak <igor.pietrzak70@gmail.com>
2022-03-12 11:22:01 +0000

    long way of task 4 overall length done

--- project2.r
+++ project2.r
@@ -41,3 +41,16 @@ histograms <- function() {
 }
 
 # TASK 4:
+# Want to know if there's a statistically significant difference between the means # nolint
+length_hypothesis <- function() {
+    ardglass_mean <- mean(ardglass_data$Overall.length)
+    newlyn_mean <- mean(newlyn_data$Overall.length)
+    ardglass_sd_length <- sd(ardglass_data$Overall.length)
+    critical_value <- 12.706
+    t <- sqrt(how_many_ardglass)*((ardglass_mean - newlyn_mean)/(ardglass_sd_length)) #nolint
+    reject_ho <- FALSE
+    if( abs(t) > critical_value) {                                                      # nolint
+        reject_ho <- TRUE
+    }
+    return(reject_ho)
+}
